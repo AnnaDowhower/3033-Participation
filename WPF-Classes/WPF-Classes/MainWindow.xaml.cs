9acacd7068c82fc80c540da53305dca6769d2657
Anna Dowhower <anna.dowhower@att.net>
2021-09-15 16:18:41 -0500

    partially done with participation

---
+++ WPF-Classes/WPF-Classes/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddToy_Click(object sender, RoutedEventArgs e)
+        {
+
+            bool isEverythingGood = true;
+
+            if (string.IsNullOrWhiteSpace(txtManufacturer.Text) == true)
+            {
+                isEverythingGood = false;
+                MessageBox.Show("You must enter a valid manufacturer");
+            }
+            if (string.IsNullOrWhiteSpace(txtName.Text) == true)
+            {
+                isEverythingGood = false;
+                MessageBox.Show("You must enter a valid toy name");
+            }
+            if (string.IsNullOrWhiteSpace(txtImage.Text) == true)
+            {
+                isEverythingGood = false;
+                MessageBox.Show("You must enter a valid image URL");
+            }
+
+            double price;
+            if (double.TryParse(txtPrice.Text, out price) == false)
+            {
+                isEverythingGood = false;
+                MessageBox.Show("You must enter a valid price");
+            }
+
+            if (isEverythingGood == false)
+            {
+                return;
+            }
+
+            Toy toy = new Toy()
+            {
+                Manufacturer = txtManufacturer.Text,
+                Name = txtName.Text,
+                Price = price,
+                Image = txtImage.Text,
+            };
+
+            lstToys.Items.Add(toy);
+
+            txtImage.Clear();
+            txtManufacturer.Clear();
+            txtName.Clear();
+            txtPrice.Clear();
+
+
+        }
+    }
+}
---
+++ WPF-Classes/WPF-Classes/Toy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes
+{
+    public class Toy
+    {
+
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+
+        private string Aisle;
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Image = string.Empty;
+            Aisle = string.Empty;
+        }
+        public Toy(string manufacturer, string name, double price)
+        {
+            Manufacturer = manufacturer;
+            Name = name;
+            Price = price;
+            Image = string.Empty;
+            Aisle = string.Empty;
+        }
+        public string GetAisle()
+        {
+            return $"{Manufacturer.ToUpper()[0]}{Price.ToString("N0")}";
+        }
+        public override string ToString()
+        {
+            return $"{Manufacturer} - {Name}";
+        }
+
+    }
+}
