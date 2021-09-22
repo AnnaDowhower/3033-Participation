097823671ea287a54801812f7aad50199c70340d
Anna Dowhower <anna.dowhower@att.net>
2021-09-22 16:01:53 -0500

    Done with participation

---
+++ Contact List/Contact List/Contact.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact_List
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+
+        public Contact()
+        {
+            Id = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Photo = string.Empty;
+        }
+        public Contact(string line)
+        {
+            var pieces = line.Split('|');
+
+            Id = 0;
+            FirstName = pieces[1];
+            LastName = pieces[2];
+            Email = pieces[3];
+            Photo = pieces[4];
+        }
+
+        public override string ToString()
+        {
+            return $"{ LastName}, { FirstName}";
+        }
+    }
+}
---
+++ Contact List/Contact List/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Contact_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Contact> Contacts = new List<Contact>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var lines = File.ReadAllLines("contacts.txt").Skip(1);
+
+            foreach (var line in lines)
+            {
+                Contacts.Add(new Contact(line));
+            }
+            PopulateListBox(Contacts);
+
+        }
+        private void PopulateListBox(List<Contact> contacts)
+        {
+            foreach (Contact person in contacts)
+            {
+                lstContacts.Items.Add(person);
+            }
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            Contact selectedPerson = (Contact)lstContacts.SelectedItem;
+            txtFname.Text = selectedPerson.FirstName;
+            txtLname.Text = selectedPerson.LastName;
+            txtEmail.Text = selectedPerson.Email;
+
+            imgPhoto.Source = new BitmapImage(new Uri(selectedPerson.Photo));
+        }
+    }
+}
