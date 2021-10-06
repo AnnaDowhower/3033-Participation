c586d52e0e5e612679efcc6b2ea99ab8d3415800
Anna Dowhower <anna.dowhower@att.net>
2021-10-06 16:14:26 -0500

    there is probably nothing correct about this

---
+++ JSON Pokemon/JSON Pokemon/Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Pokemon
+{
+    public class Pokemon
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public List<Results> results {get; set;}
+
+        public Pokemon()
+        {
+            count = 0;
+            next = string.Empty;
+            previous = string.Empty;
+            results = new List<Results>();
+        }
+        public Pokemon(List<Results> Result)
+        {
+            count = 0;
+            next = string.Empty;
+            previous = string.Empty;
+            results = Result;
+        }
+    }
+}
