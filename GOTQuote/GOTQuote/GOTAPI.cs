efdfc0a4dac281870d36c30d176b59835736891e
Anna Dowhower <anna.dowhower@att.net>
2021-10-13 15:59:59 -0500

    Done with GOT participation

---
+++ GOTQuote/GOTQuote/GOTAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOTQuote
+{
+    public class GOTAPI
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+    }
+}
---
+++ GOTQuote/GOTQuote/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GOTQuote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<GOTAPI> quotes = new List<GOTAPI>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetQuote_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = "https://got-quotes.herokuapp.com/quotes";
+                string json = client.GetStringAsync(url).Result;
+
+                GOTAPI api = JsonConvert.DeserializeObject<GOTAPI>(json);
+
+                string quote = api.quote.ToString();
+                blkQuote.Text = quote;
+                string name = api.character.ToString();
+                blkName.Text = name;
+                quotes.Add(api);
+            }
+        }
+
+        private void btnCreateFile_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (quotes.Count() < 1)
+            {
+                MessageBox.Show("There was an error creating your file");
+            }
+            else
+            {
+                string json2 = JsonConvert.SerializeObject(quotes);
+                File.WriteAllText("GOT_Quotes.json", json2);
+
+                MessageBox.Show("Your file \"GOT_Quotes.json\" was created");
+            }
+
+        }
+    }
+}
