1b0848803a0a92ef4d3b1304490580de75eed57f
Anna Dowhower <anna.dowhower@att.net>
2021-10-18 16:20:36 -0500

    almost finished

---
+++ JSON-Serialization/JSON-Serialization/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Serialization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Platform>  games = new List<Platform>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var lines = File.ReadAllLines("all_games.csv").Skip(1);
+
+            foreach (var line in lines)
+            {
+                games.Add(new Platform(line));
+            }
+
+            PopulateComboBox();
+        }
+
+        private void PopulateListBox(List<Platform> FilteredGames)
+        {
+            lstGames.Items.Clear();
+
+            foreach (var game in FilteredGames)
+            {
+                lstGames.Items.Add(game);
+            }
+        }
+
+        private List<Platform> FilterListBox(List<Platform> games)
+        {
+            string plat = cboPlatforms.SelectedValue.ToString();
+
+            List<Platform> filteredGames = new List<Platform>();
+
+            foreach (var game in games)
+            {
+                if (plat.ToLower() == "all")
+                {
+                    filteredGames.Add(game);
+                }
+                else if (game.platform.Contains(plat))
+                {
+                    filteredGames.Add(game);
+                }
+            }
+            return filteredGames;
+        }
+
+        private void PopulateComboBox()
+        {
+            cboPlatforms.Items.Add("All");
+            foreach (var game in games)
+            {
+                if (string.IsNullOrWhiteSpace(game.platform))
+                {
+                    continue;
+                }
+                if (!cboPlatforms.Items.Contains(game.platform))
+                {
+                    cboPlatforms.Items.Add(game.platform);
+                }
+            }
+        }
+
+        private void lstGames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            WndInfo wnd = new WndInfo();
+
+            wnd.Show();
+        }
+        private void UpdateDataForFilter()
+        {
+            if (cboPlatforms.SelectedValue is null)
+            {
+                return;
+            }
+            List<Platform> FilteredGames;
+            FilteredGames = FilterListBox(games);
+            PopulateListBox(FilteredGames);
+
+            PopulateListBox(FilteredGames);
+        }
+        private void cboPlatforms_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateDataForFilter();
+        }
+    }
+}
---
+++ JSON-Serialization/JSON-Serialization/Platform.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Serialization
+{
+    public class Platform
+    {
+        public string Name { get; set; }
+        public string platform { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Summary{ get; set; }
+        public string MetaScore{ get; set; }
+        public string UserReview { get; set; }
+
+        public Platform()
+        {
+            Name = string.Empty;
+            platform = string.Empty;
+            ReleaseDate = string.Empty;
+            Summary = string.Empty;
+            MetaScore = string.Empty;
+            UserReview = string.Empty;
+        }
+        public Platform(string lines)
+        {
+            var pieces = lines.Split(',');
+
+            Name = pieces[0];
+            platform = pieces[1];
+            ReleaseDate = pieces[2];
+            Summary = pieces[3];
+            MetaScore = pieces[4];
+            UserReview = pieces[5];
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
